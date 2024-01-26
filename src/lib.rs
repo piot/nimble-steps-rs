f14c65166a09f267c57aa4e25cbfda6e2198e54c
Peter Bjorklund <piot@hotmail.com>
2024-01-26 20:29:32 +0100

    chore: add header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+/*----------------------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/nimble-steps-rs
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------------------*/
 use std::collections::VecDeque;
 
 use tick_id::TickId;
